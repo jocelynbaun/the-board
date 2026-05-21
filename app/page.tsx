99908f29a5d778e7a3f4e4ba66e28ce78bf79e3f
Jocelyn Baun <rockstarbaun@gmail.com>
2026-05-21 14:51:47 -0400

    Add responsive padding adjustments for main content on larger screens and apply class to main element

--- app/page.tsx
+++ app/page.tsx
@@ -170,6 +170,7 @@ export default function Home() {
       </AnimatePresence>
 
       <main
+        className="main-content"
         style={{
           position: 'relative',
           zIndex: 3,
